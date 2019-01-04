03120932b8f8e7f8d9369717be36cf9598281c56
Valentin Lobanov <vdlutik@gmail.com>
2019-01-04 21:19:34 +0200

    1

---
+++ Infrastructure/AppContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptions<AppContext> options)
+        {
+            options.
+        }
+    }
+}
---
+++ Infrastructure/Models/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+namespace Infrastructure
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSql
+        }
+    }
+}
